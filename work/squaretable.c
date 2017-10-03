dc5ecf1b11441e7e681838042f02488252c71fb7
Ahmed <ahmed.almass@edu.sait.ca>
2017-10-03 15:33:23 -0600

    squaretable.c

---
+++ work/squaretable.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main (void)
+{
+  int i,j;
+  int array[12] = {1,2,3,4,5,6,7,8,9,10,11,12};
+
+
+  printf("$ squaretable\n");
+  printf("+----+----+-----+\n");
+
+  i = array[i]
+    j = array [j]
+  for(i = 1; i< 12; ++i){
+    printf("|  %d  |
+
+
+
+return 0;
+
+
+}
