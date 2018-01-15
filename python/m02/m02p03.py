4be0adffc2efd9628b76d6d0e0c1802a29e99d3c
ahmed almass <ahmed.almass@edu.sait.ca>
2018-01-15 08:24:52 -0700

    m02p0

---
+++ python/m02/m02p03.py
@@ -0,0 +1,14 @@
+import keyword
+
+i = 0
+j = 0
+
+keylist = keyword.kwlist
+
+for i in range(len(keylist)):
+ print keylist[i]
+ i+=1
+
+for j in reversed(range(len(keylist))):
+ print keylist[j]
+ i-=1
