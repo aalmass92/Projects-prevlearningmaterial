f68cd28a51d7fcd0fd2f0e58524d6c4501ab2be7
ahmed almass <ahmed.almass@edu.sait.ca>
2018-01-22 08:18:04 -0700

    m02p0

--- python/m02/m02p07.py
+++ python/m02/m02p07.py
@@ -1,13 +1,25 @@
-import pwd
-
 #!/usr/bin/python
 
+import subprocess
 
-#def print_user_line(username):
- #   if username == "None":
-print "+-----------+--------------------------+"
-       # return
-for p in pwd.getpwall():
-      print"| " ,p[0],"|"
+def parser(test_str):
+    start_idx = 0
+    end_idx = 0
+    user_list = []
 
-print "+-----------+--------------------------+"
+    for i in range(0,len(test_str)):
+        if(test_str[i] == '\n'):
+            end_idx = i
+            user_str = test_str[start_idx:end_idx]
+            user_list.append(user_str)
+            start_idx = i+1
+    return user_list
+
+
+output = subprocess.check_output(['ls', '/home'])
+user_list = parser(output)
+print ('+----------------------+----------------------------+')
+for username in user_list :
+       print('| %-20s | /home/%-20s |' % (username,username))
+print ('+----------------------+----------------------------+')
+
