3315c4b88962d363d82cd1b03db141fd960d5009
ahmed almass <ahmed.almass@edu.sait.ca>
2018-01-23 18:11:02 -0700

    m04p01.py

---
+++ python/m04/m04p01.py
@@ -0,0 +1,11 @@
+#!/usr/bn/python
+
+import struct os sys
+
+def processid()
+struct.unpack("i",words)[0]
+
+file_name = sys.argv[0]
+pid = int(os.getpid())
+f = open(file_name,"r")
+pid = f.read(pid)
