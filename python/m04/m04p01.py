311d59cee52044e989c19afc47aeef8461e8e7e5
ahmed almass <ahmed.almass@edu.sait.ca>
2018-01-29 23:36:59 -0700

    m04

--- python/m04/m04p01.py
+++ python/m04/m04p01.py
@@ -1,8 +1,33 @@
-#!/usr/bn/python
+import sys
+class LinuxProcess:
+    def __init__(self, pid):
+        self.file_name ="/proc/"+str(pid)+"/stat"
+        self.info_list = []
+        
+    def readfile(self):
+        f = open(self.file_name,'r')
+        filedata = f.read()
+        self.info_list = filedata.split()
 
-import struct, os, sys
+    def printStats(self):
+        self.readfile()
+        print "%20s" % "name: ", "%20s" % self.info_list[1] 
+        print "%20s" % "rss_lim: ","%20s" % hex(int(self.info_list[24])) 
+        print "%20s" % "start_code: ","%20s" % hex(int(self.info_list[25])) 
+        print "%20s" % "end_code: ", "%20s" % hex(int(self.info_list[26])) 
+        print "%20s" % "start_stack: ","%20s" % hex(int(self.info_list[27]))
+        print "%20s" % "esp: ","%20s" % hex(int(self.info_list[28])) 
+        print "%20s" % "eip: ","%20s" % hex(int(self.info_list[29])) 
+        print "%20s" % "start_data: ","%20s" % hex(int(self.info_list[44])) 
+        print "%20s" % "end_data: ","%20s" % hex(int(self.info_list[45])) 
+        print "%20s" % "start_brk: ","%20s" % hex(int(self.info_list[46])) 
+        print "%20s" % "arg_start: ","%20s" % hex(int(self.info_list[47])) 
+        print "%20s" % "arg_end: ","%20s" % hex(int(self.info_list[48])) 
+        print "%20s" % "env_start: ","%20s" % hex(int(self.info_list[49])) 
+        print "%20s" % "env_end: ","%20s" % hex(int(self.info_list[50]))
 
-asd = str(type(os.getpid))
-
-
-print asd
+if (len(sys.argv) < 2):
+    print "Error: Please provide a pid"
+else:
+    obj = LinuxProcess(int(sys.argv[1]))
+    obj.printStats()
---
+++ python/m04/m04p02.py
@@ -0,0 +1,71 @@
+import os
+import os.path
+
+class LinuxProcList:
+	def __init__(self):
+		self.pidlist = []
+		self.pstree = None
+	
+	def procList(self):
+		location = "/proc/"
+		for d in os.listdir(location):
+			if (d.isdigit()):
+				self.pidlist.append(d)
+		return self.pidlist
+
+	def cmdline(self, pid):
+		cmdfile = "/proc/" + str(pid) + "/cmdline"
+	 	if (os.path.exists(cmdfile)):
+			f = open(cmdfile, 'r')
+			cmd = f.read()
+			f.close()
+			return cmd
+		else:
+			return "None"
+ 	
+	def getPPID(self, pid):
+		statfile = "/proc/" + str(pid) + "/stat"
+		if (os.path.exists(statfile)):
+			f = open(statfile, 'r')
+			contents = f.read()
+			statlist = contents.split()
+			f.close()
+			return str(statlist[3])
+		else:
+			return ""
+		
+	def createPSTree(self):
+		self.pstree= {}		
+		for pid in self.pidlist:
+			ppid = self.getPPID(pid)
+			if (ppid != ""):
+				if ppid == '0':
+					ppid = str(pid)
+				if ppid in self.pstree:
+					self.pstree[ppid].append(pid)
+				else:
+					self.pstree[ppid] = [pid]
+			else:
+				self.pstree[pid] = [pid]
+	
+	def findChildren(self, pid):
+		if self.pstree == None:
+			self.createPSTree()
+		if pid in self.pstree:
+			return self.pstree[pid]
+		else:
+			return None
+	
+	def getPSTree(self):
+		if self.pstree == None:
+			self.createPSTree()
+		return self.pstree
+
+obj = LinuxProcList()
+print "The process list is : ",obj.procList()
+        
+process_id = raw_input("Enter the process id: ")
+print "Command line for the pid", process_id,": ", obj.cmdline(process_id)        
+
+print " The Children for the pid", process_id," ", obj.findChildren(process_id)
+
