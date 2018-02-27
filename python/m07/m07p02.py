c0dfea0bc1015a32927eec3f7ab6f503af18b73e
ahmed almass <ahmed.almass@edu.sait.ca>
2018-02-27 09:58:51 -0700

    m07p0

---
+++ python/m07/m07p02.py
@@ -0,0 +1,6 @@
+from immlib import *
+
+def main(args):
+    imm = Debugger()
+    imm.log("ahmed.almass@edu.sait.ca")
+    return "[*] PyCommand Executed!"
---
+++ python/m07/winexecalc.c
@@ -0,0 +1,5 @@
+#include <Windows.h>
+
+int main(void) {
+	WinExec("calc.exe", SW_HIDE);
+}
