376eede60486928352f1b1810d044e561534eca8
ahmed almass <ahmed.almass@edu.sait.ca>
2018-03-28 12:56:56 -0600

    m11

---
+++ python/m11/m11tcpcli.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import socket
+target_host = "itss.biomea.com"
+target_port = 80
+
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+ # connect the client
+
+client.connect((target_host,target_port))
+# send some data
+
+client.send("GET / HTTP/1.1\r\nHost: itss.biomea.com/itsc203/cgi-bin/echo.cgi\r\n\r\n")
+ 
+# receive some data
+response = client.recv(4096)
+print response
