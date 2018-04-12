ff706b185f455025e93c578d7f4c9e7103aa03d8
ahmed almass <ahmed.almass@edu.sait.ca>
2018-04-12 08:46:43 -0600

    m13p03

---
+++ python/m13/m13p03report.py
@@ -0,0 +1,39 @@
+Script started on Thu 12 Apr 2018 02:10:37 AM MDT
+]0;ahmed-a@ubuntu: ~/git/ahmed-a/python/m13ahmed-a@ubuntu:~/git/ahmed-a/python/m13$ python m13p03.py 
+[200] => http://testphp.vulnweb.com/CVS/
+[200] => http://testphp.vulnweb.com/admin/
+[200] => http://testphp.vulnweb.com/index.php
+[200] => http://testphp.vulnweb.com/index.bak
+[200] => http://testphp.vulnweb.com/search.php
+[200] => http://testphp.vulnweb.com/login.php
+[200] => http://testphp.vulnweb.com/login.php
+[200] => http://testphp.vulnweb.com/images/
+[200] => http://testphp.vulnweb.com/index.php
+[200] => http://testphp.vulnweb.com/logout.php
+[200] => http://testphp.vulnweb.com/categories.php
+[200] => http://testphp.vulnweb.com/404.php
+[200] => http://testphp.vulnweb.com/categories.php
+[200] => http://testphp.vulnweb.com/signup.php
+[200] => http://testphp.vulnweb.com/comment.php
+[200] => http://testphp.vulnweb.com/pictures/
+[200] => http://testphp.vulnweb.com/product.php
+[200] => http://testphp.vulnweb.com/guestbook.php
+[200] => http://testphp.vulnweb.com/guestbook.php
+[200] => http://testphp.vulnweb.com/cart.php
+[200] => http://testphp.vulnweb.com/userinfo.php
+[200] => http://testphp.vulnweb.com/logout.php
+[200] => http://testphp.vulnweb.com/cart.php
+!!! 403 => http://testphp.vulnweb.com/cgi-bin/
+[200] => http://testphp.vulnweb.com/Connections/
+[200] => http://testphp.vulnweb.com/crossdomain.xml
+[200] => http://testphp.vulnweb.com/_mmServerScripts/
+[200] => http://testphp.vulnweb.com/listproducts.php
+!!! 302 => http://testphp.vulnweb.com/redir.php
+[200] => http://testphp.vulnweb.com/signup.php
+[200] => http://testphp.vulnweb.com/showimage.php
+[200] => http://testphp.vulnweb.com//
+[200] => http://testphp.vulnweb.com/404.php
+]0;ahmed-a@ubuntu: ~/git/ahmed-a/python/m13ahmed-a@ubuntu:~/git/ahmed-a/python/m13$ exit
+exit
+
+Script done on Thu 12 Apr 2018 08:46:26 AM MDT
